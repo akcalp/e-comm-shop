1a21aa5039fd07a80f20536d2edab78715b03fbd
akcalp <alp92akca@gmail.com>
2020-11-20 01:44:52 +0300

    reducer, store, redux setup

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs"
+
+const users = [
+    {
+        name: "Admin User",
+        email: "admin@test.com",
+        password: bcrypt.hashSync("12345", 10),
+        isAdmin: true
+    },
+    {
+        name: "Alp Akca",
+        email: "alp@test.com",
+        password: bcrypt.hashSync("12345", 10),
+    },
+    {
+        name: "Ant Akca",
+        email: "ant@test.com",
+        password: bcrypt.hashSync("12345", 10),
+    }
+
+];
+
+export default users;
