9ec7a7edd305eb6ef9a91afd18e785d2334d9c4a
akcalp <alp92akca@gmail.com>
2020-11-27 02:29:59 +0300

    added RegisterScreen, registerActions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import {USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT} from "../constants/userConstants";
+import {
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGOUT, USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS
+} from "../constants/userConstants";
 import {PRODUCT_LIST_FAIL} from "../constants/productConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -42,3 +48,42 @@ export const logout = () => (dispatch) => {
         type: USER_LOGOUT
     })
 };
+
+export const register = (name,email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST,
+        });
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        const {data} = await axios.post(
+            "/api/users", {name,email, password}, config
+        );
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        });
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        });
+
+        localStorage.setItem("userInfo", JSON.stringify(data));
+
+    } catch (e) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message
+                : e.message,
+        });
+    }
+};
+
